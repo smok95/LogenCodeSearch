3c59144ba6df2a804e637339062770e544438359
smok95 <d0nzs00n@gmail.com>
2016-12-29 15:10:09 +0900

    first commit
    로젠택배 지점코드 검색프로그램
    (지점코드정보 15년8월자)

---
+++ LogenCode/LogenCode/LogenCodeForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace LogenCode
+{
+    partial class LogenCodeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogenCodeForm));
+            this.m_tbSearch = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabelAbout = new System.Windows.Forms.LinkLabel();
+            this.m_listview = new LogenCode.ListViewNF();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // m_tbSearch
+            // 
+            this.m_tbSearch.ImeMode = System.Windows.Forms.ImeMode.Hangul;
+            this.m_tbSearch.Location = new System.Drawing.Point(53, 3);
+            this.m_tbSearch.Name = "m_tbSearch";
+            this.m_tbSearch.Size = new System.Drawing.Size(131, 21);
+            this.m_tbSearch.TabIndex = 0;
+            this.m_tbSearch.TextChanged += new System.EventHandler(this.m_tbSearch_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.linkLabelAbout);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.m_tbSearch);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(661, 27);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "검색어";
+            // 
+            // linkLabelAbout
+            // 
+            this.linkLabelAbout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabelAbout.AutoSize = true;
+            this.linkLabelAbout.Location = new System.Drawing.Point(572, 9);
+            this.linkLabelAbout.Name = "linkLabelAbout";
+            this.linkLabelAbout.Size = new System.Drawing.Size(77, 12);
+            this.linkLabelAbout.TabIndex = 4;
+            this.linkLabelAbout.TabStop = true;
+            this.linkLabelAbout.Text = "프로그램안내";
+            this.linkLabelAbout.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelAbout_LinkClicked);
+            // 
+            // m_listview
+            // 
+            this.m_listview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.m_listview.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.m_listview.Font = new System.Drawing.Font("DotumChe", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.m_listview.Location = new System.Drawing.Point(0, 30);
+            this.m_listview.Name = "m_listview";
+            this.m_listview.Size = new System.Drawing.Size(661, 386);
+            this.m_listview.TabIndex = 1;
+            this.m_listview.UseCompatibleStateImageBehavior = false;
+            this.m_listview.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "분류";
+            this.columnHeader1.Width = 40;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "코드";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "권역";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "지역";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "관할지역";
+            this.columnHeader5.Width = 400;
+            // 
+            // LogenCodeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 416);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.m_listview);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "LogenCodeForm";
+            this.Text = "로젠택배 지점코드 검색";
+            this.Load += new System.EventHandler(this.LogenCodeForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListViewNF m_listview;
+        //private System.Windows.Forms.ListView m_listview;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.TextBox m_tbSearch;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabelAbout;
+    }
+}
+
---
+++ LogenCode/LogenCode/LogenCodeForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Diagnostics;
+
+namespace LogenCode
+{
+    public partial class LogenCodeForm : Form
+    {
+        SQLiteConnection m_dbConn = null;
+        public LogenCodeForm()
+        {
+            InitializeComponent();
+
+            this.Text += " (2015년 8월 현재)";
+            //SQLiteConnection.CreateFile("LogenBranchCode.sqlite");
+            m_dbConn = new SQLiteConnection("Data Source=LogenBranchCode.sqlite;Version=3;");
+            m_dbConn.Open();
+        }
+
+        ~LogenCodeForm()
+        {
+            m_dbConn.Close();
+        }
+
+        private void m_tbSearch_TextChanged(object sender, EventArgs e)
+        {
+            // UI갱신 중지
+            m_listview.BeginUpdate();
+            m_listview.Items.Clear();
+            
+            string sql = "select category, code, region1,region2,area,color from codelist where region2 like '%" + m_tbSearch.Text + "%' or area like '%" + m_tbSearch.Text + "%'";
+            SQLiteCommand cmd = new SQLiteCommand(sql, m_dbConn);
+            SQLiteDataReader rd = cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                Console.WriteLine((string)rd[5]);
+                ListViewItem item = new ListViewItem((string)rd[0]);
+                item.SubItems.Add(rd[1].ToString());
+                item.SubItems.Add((string)rd[2]);
+                item.SubItems.Add((string)rd[3]);
+                item.SubItems.Add((string)rd[4]);
+                string rgb = (string)rd[5];
+                Debug.Print(rgb);
+                item.BackColor = Color.FromArgb(
+                    Convert.ToInt32(rgb.Substring(0,2), 16),
+                    Convert.ToInt32(rgb.Substring(2,2),16),
+                    Convert.ToInt32(rgb.Substring(4,2), 16)
+                    );
+                //item.SubItems.Add(rd[5]);
+                m_listview.Items.Add(item);
+            }
+            m_listview.EndUpdate();
+        }
+
+        private void LogenCodeForm_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = m_tbSearch;
+        }
+
+        private void linkLabelAbout_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string msg =
+
+                "수기로 송장을 작성하시는 분들을 위해 제작하였습니다.\r\n" +
+                "불편한 점이나 개선사항은 아래 메일로 연락주시면 반영하겠습니다.\r\n" +
+                "\r\nd0nzs00n@gmail.com\r\n" +
+                "\r\n사용법\r\n" +
+                "예) '인천시 남동구 만수1동' \r\n" +
+                "만수1동 -> 남동구 -> 인천 순서로 검색하시면 됩니다.";
+            MessageBox.Show(msg);
+        }
+    }
+}
